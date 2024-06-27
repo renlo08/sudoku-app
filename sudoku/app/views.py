089446366dd1d9ba2f80e14a744accba2895a337
Laurent Bihin <lolofanf1@hotmail.com>
2024-06-27 21:13:58 +0200

    prepare the htmx upload view

--- sudoku/app/views.py
+++ sudoku/app/views.py
@@ -1,16 +1,17 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 from app.forms import ImageForm
 from app.models import Image
 
 
 def upload_view(request):
-    if request.method == 'POST':
-        form = ImageForm(request.POST, request.FILES)
-        if form.is_valid():
-            new_image = Image(image=request.FILES['file'])
-            new_image.save()
-    else:
-        form = ImageForm()
-    return render(request, 'process.html', context={'process_context': 'frwegfwef'})
-    # return redirect('home', context={'form': form})
+    if request.htmx:
+        if request.method == 'POST':
+            form = ImageForm(request.POST, request.FILES)
+            if form.is_valid():
+                new_image = Image(image=request.FILES['file'])
+                new_image.save()
+            else:
+                form = ImageForm()
+            render(request, 'app/partials/upload.html', context={'form': form})
+    return redirect('home')
